f6d4b2d712941f7d5c094dbf865e0f245cdf9850
Aleksandr Zotov <szotov223@gmail.com>
2023-01-07 21:15:40 +0500

    ver 1.0.3

--- src/App.js
+++ src/App.js
@@ -1,63 +1,88 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Block } from './Block'
+import React, { useEffect, useState } from 'react'
 import './index.scss'
+import { Collection } from './Collection'
+
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+]
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState('USD')
-  const [toCurrency, setToCurrency] = useState('RUB')
-  const [fromPrice, setFromPrice] = useState(1)
-  const [toPrice, setToPrice] = useState(0)
-
-  const ratesRef = useRef({})
+  const [categoryId, setCategoryId] = useState(0)
+  const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(true)
+  const [collections, setCollections] = useState([])
+  const [searchValue, setSearchValue] = useState('')
 
   useEffect(() => {
-    fetch('https://cdn.cur.su/api/latest.json')
+    setIsLoading(true)
+
+    const category = categoryId ? `category=${categoryId}` : ''
+
+    fetch(
+      `https://6353f406e64783fa827c10b0.mockapi.io/photo_collections?page=${page}&limit=2&${category}`
+    )
       .then((res) => res.json())
       .then((json) => {
-        ratesRef.current = json.rates
-        onChangeFromPrice(1)
+        setCollections(json)
       })
       .catch((err) => {
         console.warn(err)
-        alert('Чета не так')
+        alert('Ошибка при получении данных')
       })
-  }, [])
-
-  const onChangeFromPrice = (value) => {
-    const price = value / ratesRef.current[fromCurrency]
-    const result = price * ratesRef.current[toCurrency]
-    setToPrice(result.toFixed(3))
-    setFromPrice(value)
-  }
-  const onChangeToPrice = (value) => {
-    const result =
-      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
-    setFromPrice(result.toFixed(3))
-    setToPrice(value)
-  }
-
-  useEffect(() => {
-    onChangeFromPrice(fromPrice)
-  }, [fromCurrency])
-
-  useEffect(() => {
-    onChangeToPrice(toPrice)
-  }, [toCurrency])
+      .finally(() => setIsLoading(false))
+  }, [categoryId, page])
 
   return (
     <div className="App">
-      <Block
-        value={fromPrice}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrency}
-        onChangeValue={onChangeFromPrice}
-      />
-      <Block
-        value={toPrice}
-        currency={toCurrency}
-        onChangeCurrency={setToCurrency}
-        onChangeValue={onChangeToPrice}
-      />
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, i) => (
+            <li
+              className={categoryId === i ? 'active' : ''}
+              key={obj.name}
+              onClick={() => {
+                setCategoryId(i)
+              }}
+            >
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
+      </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идет загрузка...</h2>
+        ) : (
+          collections
+            .filter((obj) =>
+              obj.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} images={obj.photos} />
+            ))
+        )}
+      </div>
+      <ul className="pagination">
+        {[...Array(5)].map((_, i) => (
+          <li
+            onClick={() => setPage(i + 1)}
+            className={page === i + 1 ? 'active' : ''}
+          >
+            {i + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
