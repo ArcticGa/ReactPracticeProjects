405f1f7e9b798c7e249171bf7eccb6618a85a87b
Aleksandr Zotov <szotov223@gmail.com>
2023-01-07 21:14:56 +0500

    ver 1.0.2

--- src/App.js
+++ src/App.js
@@ -1,63 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Block } from './Block'
+import React, { useState } from 'react'
+import { ModalWindow } from './ModalWindow'
 import './index.scss'
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState('USD')
-  const [toCurrency, setToCurrency] = useState('RUB')
-  const [fromPrice, setFromPrice] = useState(1)
-  const [toPrice, setToPrice] = useState(0)
-
-  const ratesRef = useRef({})
-
-  useEffect(() => {
-    fetch('https://cdn.cur.su/api/latest.json')
-      .then((res) => res.json())
-      .then((json) => {
-        ratesRef.current = json.rates
-        onChangeFromPrice(1)
-      })
-      .catch((err) => {
-        console.warn(err)
-        alert('Чета не так')
-      })
-  }, [])
-
-  const onChangeFromPrice = (value) => {
-    const price = value / ratesRef.current[fromCurrency]
-    const result = price * ratesRef.current[toCurrency]
-    setToPrice(result.toFixed(3))
-    setFromPrice(value)
-  }
-  const onChangeToPrice = (value) => {
-    const result =
-      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
-    setFromPrice(result.toFixed(3))
-    setToPrice(value)
-  }
-
-  useEffect(() => {
-    onChangeFromPrice(fromPrice)
-  }, [fromCurrency])
-
-  useEffect(() => {
-    onChangeToPrice(toPrice)
-  }, [toCurrency])
+  const [openModal, setOpenModal] = useState(false)
 
   return (
     <div className="App">
-      <Block
-        value={fromPrice}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrency}
-        onChangeValue={onChangeFromPrice}
-      />
-      <Block
-        value={toPrice}
-        currency={toCurrency}
-        onChangeCurrency={setToCurrency}
-        onChangeValue={onChangeToPrice}
-      />
+      <button className="open-modal-btn" onClick={() => setOpenModal(true)}>
+        ✨ Открыть окно
+      </button>
+      {openModal && (
+        <ModalWindow setOpenModal={setOpenModal}>
+          <h2>Это модалка :|</h2>
+        </ModalWindow>
+      )}
     </div>
   )
 }
