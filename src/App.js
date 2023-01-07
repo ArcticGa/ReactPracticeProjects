d730bda5ddfc768737a761b422f033b2ecae4c94
Aleksandr Zotov <szotov223@gmail.com>
2023-01-07 21:10:33 +0500

    ver 1.0.1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { Block } from './Block'
 import './index.scss'
-import { useState } from 'react'
 
 function App() {
-  const [value, setValue] = useState(0)
+  const [fromCurrency, setFromCurrency] = useState('USD')
+  const [toCurrency, setToCurrency] = useState('RUB')
+  const [fromPrice, setFromPrice] = useState(1)
+  const [toPrice, setToPrice] = useState(0)
+
+  const ratesRef = useRef({})
+
+  useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates
+        onChangeFromPrice(1)
+      })
+      .catch((err) => {
+        console.warn(err)
+        alert('Чета не так')
+      })
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result.toFixed(3))
+    setFromPrice(value)
+  }
+  const onChangeToPrice = (value) => {
+    const result =
+      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+    setFromPrice(result.toFixed(3))
+    setToPrice(value)
+  }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{value}</h1>
-        <button className="minus" onClick={() => setValue(value - 1)}>
-          - Минус
-        </button>
-        <button className="plus" onClick={() => setValue(value + 1)}>
-          Плюс +
-        </button>
-      </div>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   )
 }
