1b63ecc5d2f82adfa57d0f1f7b14517a59100f01
Aleksandr Zotov <szotov223@gmail.com>
2023-01-07 21:07:47 +0500

    ver 1.0

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './index.scss'
+import { useState } from 'react'
+
+function App() {
+  const [value, setValue] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{value}</h1>
+        <button className="minus" onClick={() => setValue(value - 1)}>
+          - Минус
+        </button>
+        <button className="plus" onClick={() => setValue(value + 1)}>
+          Плюс +
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
